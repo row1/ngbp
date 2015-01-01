cb205ae5668244a1e1a284c72679f89a98ef81b1
row1 <mcphd101@hotmail.com>
2015-01-01 17:25:46 +1300

    Support for Less.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@
 /*jslint node: true */
 'use strict';
 var gulp = require('gulp'),
+  concat = require('gulp-concat'),
   del = require('del'),
   merge = require('merge-stream'),
   concat = require('gulp-concat'),
   jshint = require('gulp-jshint'),
   html2js = require('gulp-html2js'),
+  less = require('gulp-less'),
   wrap = require("gulp-wrap"),
   userConfig = require('./build.config.js'),
+  packageInfo = require('./package.json'),
   templates = [{base: 'src/app', src: userConfig.app_files.atpl, module: 'templates-app', dest: '/templates-app.js'},
                {base: 'src/common', src: userConfig.app_files.ctpl, module: 'templates-common', dest: '/templates-common.js'}];
 
@@ -40,6 +43,14 @@ gulp.task('scripts', ['clean'], function () {
     .pipe(gulp.dest(userConfig.build_dir + '/src'));
 });
 
-gulp.task('default', ['clean', 'templates', 'scripts'], function () {
+gulp.task('styles', ['clean'], function () {
+  return gulp.src(userConfig.app_files.less)
+    .pipe(less())
+    .pipe(concat(packageInfo.name + '-' + packageInfo.version + '.css'))
+    .pipe(gulp.dest(userConfig.build_dir + '/assets'));
+});
+
+
+gulp.task('default', ['clean', 'templates', 'scripts', 'styles'], function () {
   //console.log(userConfig);
 });
