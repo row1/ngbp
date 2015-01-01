ca6f26812eb4abe989acdcdcaad2e26cec5ad390
row1 <mcphd101@hotmail.com>
2015-01-01 15:05:48 +1300

    Added jshint. Template task now returns a merged stream.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@
 'use strict';
 var gulp = require('gulp'),
   del = require('del'),
+  merge = require('merge-stream'),
   concat = require('gulp-concat'),
+  jshint = require('gulp-jshint'),
   html2js = require('gulp-html2js'),
+  wrap = require("gulp-wrap"),
   userConfig = require('./build.config.js'),
   templates = [{base: 'src/app', src: userConfig.app_files.atpl, dest: '/templates-app.js'},
                {base: 'src/common', src: userConfig.app_files.ctpl, dest: '/templates-common.js'}];
@@ -14,19 +17,28 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('templates', ['clean'], function () {
+  var calls = [];
   templates.forEach(function (template) {
-    gulp.src(template.src)
+    var call = gulp.src(template.src)
       .pipe(html2js({
         useStrict: true,
         base: template.base
       }))
       .pipe(concat(template.dest))
       .pipe(gulp.dest(userConfig.build_dir));
+    calls.push(call);
   });
+  return merge.apply(this, calls);
+});
 
+gulp.task('scripts', ['clean'], function () {
+  return gulp.src(userConfig.app_files.js)
+    .pipe(wrap('(function(){ "use strict"; <%= contents %> })();'))
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'));
 });
 
 
-gulp.task('default', ['clean', 'templates'], function () {
-  console.log(userConfig);
+gulp.task('default', ['clean', 'templates', 'scripts'], function () {
+  //console.log(userConfig);
 });
