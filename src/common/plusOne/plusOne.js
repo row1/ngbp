ec3d387545f6a1819d254f979c5bd5170bdec30d
row1 <mcphd101@hotmail.com>
2015-01-07 21:56:34 +1300

    Updated urls and travis file.

--- src/common/plusOne/plusOne.js
+++ src/common/plusOne/plusOne.js
@@ -1,15 +1,12 @@
-angular.module( 'plusOne', [] )
+angular.module('plusOne', [])
 
-.directive( 'plusOne', function() {
-  return {
-    link: function( scope, element, attrs ) {
-      gapi.plusone.render( element[0], {
-        "size": "medium",
-        "href": "http://bit.ly/ngBoilerplate"
-      });
-    }
-  };
-})
-
-;
-
+  .directive('plusOne', function () {
+    return {
+      link: function (scope, element, attrs) {
+        gapi.plusone.render(element[0], {
+          "size": "medium",
+          "href": "https://github.com/row1/ngbpgp"
+        });
+      }
+    };
+  });
